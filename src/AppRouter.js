fa9aa449f3d58ba75229d381c89a05e9a495bb7e
shilili1 <shilili1@jd.com>
2019-10-12 18:33:19 +0800

    feat: complete 路由动态传值

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
 
+
 // 首页
 function Index() {
-  return(<div>首页</div>)
+
+  const [list] = useState([
+    {id: 1, title: '文章1'},
+    {id: 2, title: '文章2'},
+    {id: 3, title: '文章3'}
+  ]);
+
+  return(
+    <ul>
+      {list.map(item => {
+        return(<li key={item.id}><Link to={`/list/${item.id}`}>item.title</Link></li>)
+      })}
+    </ul>
+  )
 }
 
 // 列表
-function List() {
-  return(<div>列表</div>)
+function List(props) {
+
+  const [id, setId] = useState('');
+  useEffect(() => {
+    setId(props.match.params.id);
+    console.log(id, '我是id');
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return(<div>列表{id}</div>)
 }
 
 function AppRouter()  {
@@ -23,11 +45,11 @@ function AppRouter()  {
           <Link to="/">首页</Link>
         </li>
         <li>
-          <Link to="/list/">列表页</Link>
+          <Link to="/list/123">列表页</Link>
         </li>
       </ul>
       <Route path="/" exact component={Index} />
-      <Route path="/list/" component={List} />
+      <Route path="/list/:id" component={List} />
     </Router>
   );
 }
