6c5578fc5b5526a4c6b75b8a923f56b3058eba47
shilili1 <shilili1@jd.com>
2019-10-12 18:09:19 +0800

    feat: 完成简单的router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,25 +3,30 @@ import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-
+// 首页
 function Index() {
-  return <div>首页</div>;
+  return(<div>首页</div>)
 }
 
+// 列表
 function List() {
-  return <div>列表页面</div>;
+  return(<div>列表</div>)
 }
 
-function AppRouter() {
+function AppRouter()  {
   return (
     <Router>
       <ul>
-        <li><Link to="/">首页</Link></li>
-        <li><Link to="/list/">列表页</Link></li>
+        <li>
+          <Link to="/">首页</Link>
+        </li>
+        <li>
+          <Link to="/list/">列表页</Link>
+        </li>
       </ul>
-      <Route path="/" exact={1} component={Index} />
+      <Route path="/" exact component={Index} />
       <Route path="/list/" component={List} />
     </Router>
   );
